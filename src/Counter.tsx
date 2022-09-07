c49e0e7861084700ab551b2c23373476e3ea8db7
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2022-09-07 12:07:03 +0500

    cleaned example

---
+++ src/Counter.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+import styles from './Counter.module.css';
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          aria-label="Decrement value"
+          onClick={() => setCount(prev => prev + 1)}
+        >
+          -
+        </button>
+        <span className={styles.value}>{count}</span>
+        <button
+          className={styles.button}
+          aria-label="Increment value"
+          onClick={() => setCount(prev => prev - 1)}
+        >
+          +
+        </button>
+      </div>
+      {/* <div className={styles.row}>
+      </div> */}
+    </div>
+  );
+}
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from '../counter/counterSlice';
+import testReducer from './testSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    test: testReducer
   },
 });
 
